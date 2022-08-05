912b8aec9ce4cd99e1d68bd0fb4f45b72b56caad
infuman69 <saharoy.soham123@gmail.com>
2022-08-06 03:44:59 +0530

    cards added

--- js/homePageData.js
+++ js/homePageData.js
@@ -1,35 +1,33 @@
-const data = {
-  web: {
+let cards = document.getElementById('cards')
+
+const data = [
+  {
     title: "Web Development",
     desc: "Websites",
     more_desc: "Build with us",
-    img: ["assets\react-icon.png", "assetsmongodb.png"],
+    img: ["./assets/react-icon.png", "./assets/mongodb.png"],
   },
-  apps: {
+  {
     title: "App Development",
     desc: "Apps",
     more_desc: "Build with us",
-    img: ["assets\flutter.png", "assetskotlin.png"],
+    img: ["./assets/flutter.png", "./assets/kotlin.png"],
   },
-  design: {
-    title: "Design",
-    desc: "Design",
-    more_desc: "Build with us",
-  },
-};
+]
 
-for (key in data) {
-  var v = (document.createElement(
-    "div"
-  ).innerHTML += `<div class="infocontainterparent" style="margin-top:4vh ;">
-    <div class="infocontainer">
-      <div class="infoheading">${data[key]["title"]}</div>
-      <div class="infodescription"><br><br>${data[key]["desc"]}</div>
-      <div class="build-button"><span class="build text">${data[key]["more_desc"]}</span></div>
-        <div class="iconholder"><div class="iconone"><img src=${data[key]["img"][0]}  alt="" class="icons"></div>
-        <div class="icontwo"><img src=${data[key]["img"][1]} alt="" class="icons"></div></div>
-    </div>
-  </div>`);
+let str = ""
 
-  document.getElementById("cards").appendChild(v);
-}
+data.forEach((item) => {
+  str += `<div class="infocontainterparent">
+  <div class="infocontainer">
+    <div class="infoheading">${item.title}</div>
+    <div class="infodescription">${item.desc}</div>
+    <div class="build-button"><span class="build text">${item.more_desc}</span></div>
+      <div class="iconholder"><div class="iconone"><img src=${item.img[0]}  alt="" class="icons"></div>
+      <div class="icontwo"><img src=${item.img[1]} alt="" class="icons"></div></div>
+  </div>
+</div>`
+})
+
+cards.innerHTML = str
+
