9c678e09d94b2948ddee8099418c518b948811aa
ADLM10 <de.alimpan5@gmail.com>
2022-08-06 05:06:50 +0530

    navbar_done

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
+const navbox = document.getElementById('navshow')
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
+
   if(!menuOpen) {
+    navbox.classList.add('show')
     menuBtn.classList.add('open');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
+    navbox.classList.remove('show')
     menuOpen = false;
   }
 });
