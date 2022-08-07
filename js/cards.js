5a9a2323810d0abd0fc50eb5488d3bdedf2b979f
Shourya Shikhar <danger.ahead@pm.me>
2022-08-08 03:38:03 +0530

    added email.js, fixed some ui

--- js/cards.js
+++ js/cards.js
@@ -2,21 +2,18 @@ let cards = document.getElementById('cards')
 
 const data = [
   {
-    title: "Web Development",
+    title: "Deploy wih us",
     desc: "Websites",
-    more_desc: "Build with us",
     img: ["./assets/react-icon.png", "./assets/mongodb.png" , "./assets/html.png", "./assets/css.png", "./assets/nodejs.png" , "./assets/wordpress.png"],
   },
   {
-    title: "App Development",
+    title: "Build with us",
     desc: "Apps",
-    more_desc: "Deploy with us",
     img: ["./assets/flutter.png", "./assets/kotlin.png", "./assets/android.png", "./assets/swift.png", "./assets/firebase.png", "./assets/androidstudio.png" ],
   },
   {
-    title: "Graphics Designing",
+    title: "Design with us",
     desc: "Graphics",
-    more_desc: "Design with us",
     img: ["./assets/figma.png", "./assets/photoshop.png", "./assets/after effects.png", "./assets/adobe xd.png" , "./assets/adobe premiere pro.png", "./assets/adobe illustrator.png"],
   }
 ]
@@ -26,13 +23,11 @@ let str = ""
 data.forEach((item) => {
   str += `
     <div class="infocontainer">
-        <div class="textholder">
-            <h2 class="infoheading">${item.title}</h2>
-            <p class="infodes">${item.desc}
-            </p>
+      <div class="textholder">
+          <h2 class="infoheading">${item.title}</h2>
+          <p class="infodes">${item.desc}</p>
       </div>
       <div class="btnandicons">
-        <button class="buildbtn">${item.more_desc}</button>
         <div class="iconsmarquee">
             <div class="marqueeholder">
                 <div class="iconholder"><img src="${item.img[0]}" alt="" class="icons"></div>
