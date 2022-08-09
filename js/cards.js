93c44e33fbec2e66028b0c873d3cc4534a8b4e76
ADLM10 <de.alimpan5@gmail.com>
2022-08-09 05:38:02 +0530

    nabvar completed

--- js/cards.js
+++ js/cards.js
@@ -1,24 +1,29 @@
 let cards = document.getElementById('cards')
 
+const web = document.getElementById('BuildWeb')
+const app = document.getElementById('DeployApp')
+const design = document.getElementById('Design')
+const contacform = document.getElementById('contact')
+
 const data = [
   {
     title: "Web",
-    desc: `“Websites promote you 24/7: No employee will do that.”
-    <br><span class="rightalign">Paul Cookson</span>
-    <br>Let us help you build websites that SELL!`,
-    moredes: "Build with us",
+    desc: `<div class="infoquote">“Websites promote you 24/7: No employee will do that.”</div>
+    <div class="right">-Paul Cookson</div>
+    <div class="motto">Let us help you build websites that SELL!</div>`,
+    moredes: `<a class="buildbtnlink" id="BuildWeb" href="#contact">Build with us</a>`,
     img: ["./assets/react-icon.png", "./assets/mongodb.png" , "./assets/html.png", "./assets/css.png", "./assets/nodejs.png" , "./assets/wordpress.png"],
   },
   {
     title: "App",
     desc: "Apps",
-    moredes: "Deploy with us",
+    moredes: `<a class="buildbtnlink" id="DeployApp" href="#contact">Deploy with us</a>`,
     img: ["./assets/flutter.png", "./assets/kotlin.png", "./assets/android.png", "./assets/swift.png", "./assets/firebase.png", "./assets/androidstudio.png" ],
   },
   {
     title: "Graphics",
     desc: "Graphics",
-    moredes: "Design with us",
+    moredes: `<a class="buildbtnlink" id="Design" href="#contact">Design with us</a>`,
     img: ["./assets/figma.png", "./assets/photoshop.png", "./assets/after effects.png", "./assets/adobe xd.png" , "./assets/adobe premiere pro.png", "./assets/adobe illustrator.png"],
   }
 ]
@@ -30,7 +35,7 @@ data.forEach((item) => {
     <div class="infocontainer">
       <div class="textholder">
           <h2 class="infoheading">${item.title}</h2>
-          <p class="infodes">${item.desc}</p>
+          <div class="infodes">${item.desc}</div>
       </div>
       <div class="btnandicons">
       <button class="buildbtn">${item.moredes}</button>
@@ -50,7 +55,10 @@ data.forEach((item) => {
 `
 })
 
-cards.innerHTML = str
 
+
+
+
+cards.innerHTML = str
 
 
--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navbox = document.getElementById('navshow')
+const navbar = document.getElementById('navcolour')
 let menuOpen = false;
+
+
+
+
 menuBtn.addEventListener('click', () => {
 
   if(!menuOpen) {
-    navbox.classList.add('show')
+    navbox.classList.add('show');
     menuBtn.classList.add('open');
+    navbar.classList.add('green');
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
-    navbox.classList.remove('show')
+    navbox.classList.remove('show');
+    navbar.classList.remove('green');
     menuOpen = false;
   }
 });
+
